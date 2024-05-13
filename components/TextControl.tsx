4b6df475095385cb3bb405c97a32aa1196f96270
Carles Andres <carlos.andres.c@gmail.com>
2024-05-13 22:44:06 +0100

    Improved dropdown

--- components/TextControl.tsx
+++ components/TextControl.tsx
@@ -1,27 +1,26 @@
 import React from 'react';
+import { Input } from './ui/input';
+import { Label } from './ui/label';
 
-const TextControl = (props) => {
-  const { label, type = 'text', value, onChange, ...other } = props;
+interface TextControlProps
+  extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'onChange'> {
+  label: string;
+  onChange: (value: any, name: string) => void;
+}
+
+const TextControl = (props: TextControlProps) => {
+  const { label, onChange, ...other } = props;
   const name = label.replace(/ /g, '').toLowerCase();
 
-  const change = (event) => {
+  const change = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { value, name } = event.currentTarget;
     onChange(value, name);
   };
 
   return (
-    <div className="flex items-center space-x-2">
-      <label className=""
-        htmlFor={name}>{`${label}:`}</label>
-      <input
-        type={type}
-        id={name}
-        name={name}
-        onChange={change}
-        value={value}
-        className="p-1 rounded-sm"
-        {...other}
-      />
+    <div className="grid gap-3">
+      <Label className="" htmlFor={name}>{`${label}:`}</Label>
+      <Input id={name} name={name} onChange={change} {...other} />
     </div>
   );
 };
